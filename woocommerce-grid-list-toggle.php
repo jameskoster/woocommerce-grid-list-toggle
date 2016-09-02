6fe2447c8ed291348c5d19a620fecda3d32d7ecd
James Koster <james@jameskoster.co.uk>
2016-09-02 12:50:28 +0100

    tested up to

--- woocommerce-grid-list-toggle.php
+++ woocommerce-grid-list-toggle.php
@@ -7,7 +7,7 @@ Version: 1.0.0
 Author: jameskoster
 Author URI: http://jameskoster.co.uk
 Requires at least: 4.0
-Tested up to: 4.2
+Tested up to: 4.6
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 Text Domain: woocommerce-grid-list-toggle
